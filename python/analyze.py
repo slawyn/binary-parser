8708874c240d24be09644d0118dc0c189a3c518c
slawyn <unixtracer@gmail.com>
2022-11-15 23:45:59 +0100

    FIX python pe-parser strings

--- python/analyze.py
+++ python/analyze.py
@@ -6,20 +6,18 @@ from parsers.elf import ElfParser
 from utils import log, load_file
 
 
-if __name__=="__main__":
-	if len(sys.argv)>=3:
-		in_filename = sys.argv[1]
-		out_filename = sys.argv[2]
-		out_tempfolder = sys.argv[3]
+if __name__ == "__main__":
+    if len(sys.argv) >= 1:
+        in_filename = sys.argv[1]
 
-		try :
-			buffer = load_file(in_filename)
-			if buffer[0:4] == b"\x7fELF":
-				parser = ElfParser(buffer)
-				parser.print_all()
-			elif buffer[0:2] == b"MZ":
-				parser = PeParser(buffer)
-				parser.print_all()
+        try:
+            buffer = load_file(in_filename)
+            if buffer[0:4] == b"\x7fELF":
+                parser = ElfParser(buffer)
+                parser.print_all()
+            elif buffer[0:2] == b"MZ":
+                parser = PeParser(buffer)
+                parser.print_all()
 
-		except Exception as e:
-			log(e)
+        except Exception as e:
+            log(e)
--- python/utils.py
+++ python/utils.py
@@ -2,84 +2,101 @@ import struct
 import datetime
 import os
 
+
 def log(s):
     """Logging function
     """
-    print("%s ## %s"%(datetime.datetime.now().time(),s))
+    print("%s ## %s" % (datetime.datetime.now().time(), s))
 
 
 def write_file(filename, buffer):
-	"""Write buffer into file
-	"""
-	if os.path.isfile(filename):
-		os.remove(filename)
+    """Write buffer into file
+    """
+    if os.path.isfile(filename):
+        os.remove(filename)
 
-	with open(filename, "wb") as f:
-		f.write(buffer)
-		f.close()
+    with open(filename, "wb") as f:
+        f.write(buffer)
+        f.close()
 
-	log( "[%s] Written Bytes: %s" %(filename, len(buffer)))
+    log("[%s] Written Bytes: %s" % (filename, len(buffer)))
 
 
 def load_file(filename):
-	"""Load binary file into buffer
-	"""
-	bytes = []
-	with open(filename,"rb") as f:
-		bytes = f.read()
+    """Load binary file into buffer
+    """
+    bytes = []
+    with open(filename, "rb") as f:
+        bytes = f.read()
 
-	log( "[%s] Read Bytes: %s" %(filename, len(bytes)))
-	return bytes
+    log("[%s] Read Bytes: %s" % (filename, len(bytes)))
+    return bytes
 
 
 def unpack_str(buffer):
-	return struct.unpack("s", buffer)
+    return struct.unpack("s", buffer)
 
-STRUCT_FORMAT = {1:"B", 2:"H", 4:"I", 8:"Q"}
+
+STRUCT_FORMAT = {1: "B", 2: "H", 4: "I", 8: "Q"}
+
+
 def unpack(buffer, big_endian):
-	"""Unpack buffer
-	"""
-	idx = len(buffer)
-	if big_endian:
-		return struct.unpack(">" + STRUCT_FORMAT[idx], buffer)[0]
-	return struct.unpack("<" + STRUCT_FORMAT[idx], buffer)[0]
+    """Unpack buffer
+    """
+    idx = len(buffer)
+    if big_endian:
+        return struct.unpack(">" + STRUCT_FORMAT[idx], buffer)[0]
+    return struct.unpack("<" + STRUCT_FORMAT[idx], buffer)[0]
+
 
 def unpackuint8(data):
-	return struct.unpack("<B",data)[0]
+    return struct.unpack("<B", data)[0]
+
 
 def unpackint8(data):
-	return struct.unpack("<b",data)[0]
+    return struct.unpack("<b", data)[0]
+
 
 def unpackuint16(data):
-	return struct.unpack("<H",data)[0]
+    return struct.unpack("<H", data)[0]
+
 
 def unpackint16(data):
-	return struct.unpack("<h",data)[0]
+    return struct.unpack("<h", data)[0]
+
 
 def unpackuint32(data):
-	return struct.unpack("<I",data)[0]
+    return struct.unpack("<I", data)[0]
+
 
 def unpackint32(data):
-	return struct.unpack("<i",data)[0]
+    return struct.unpack("<i", data)[0]
+
 
 def unpackuint64(data):
-	return struct.unpack("<Q",data)[0]
+    return struct.unpack("<Q", data)[0]
+
 
 def unpackint64(data):
-	return struct.unpack("<q",data)[0]
+    return struct.unpack("<q", data)[0]
+
 
 def readstring(data, offset):
-	i=0
-	string = ""
-	while(data[offset+i]!=0):
-		i+=1
-		string+=chr(data[offset+i])
+    i = 0
+    string = ""
+    while (data[offset + i] != 0):
+        if data[offset + i] >= 32 and data[offset+i] <= 126:
+            string += chr(data[offset + i])
+        else:
+            string += str(data[offset + i])
+        i += 1
 
-	return string
+    return string
+
 
 def convertRVAToOffset(address, sections):
-	for section in sections:
-		if address>= section.VirtualAddress and \
-		   address<=(section.VirtualAddress+section.VirtualSize):
+    for section in sections:
+        if address >= section.VirtualAddress and \
+           address <= (section.VirtualAddress+section.VirtualSize):
 
-			return (address - section.VirtualAddress) + section.PointerToRawData
+            return (address - section.VirtualAddress) + section.PointerToRawData
